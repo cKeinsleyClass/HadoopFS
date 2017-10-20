2ec868c12b983571cc41424f900b2e0d7be881d8
DavisGilmore <davisgilmore@yahoo.com>
2017-10-20 14:54:00 -0400

    added prints

--- CreateTables.java
+++ CreateTables.java
@@ -18,14 +18,19 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class CreateTables {
 	
 	public static void main(String[] args) throws IOException {
+		System.out.println("starting hbase");
 		Configuration conf = HBaseConfiguration.create();
 		Connection connection = ConnectionFactory.createConnection(conf);
 		HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
+		
+		System.out.println("have admin");
 		
 		HTableDescriptor tableDes = new HTableDescriptor(TableName.valueOf("java_test_table"));
 		tableDes.addFamily(new HColumnDescriptor("test"));
 		
 		admin.createTable(tableDes);
+		
+		System.out.println("made table test");
 		
 		Table table = connection.getTable(TableName.valueOf("test"));
 		
