ecf7fadab15609eac8b0eff4b2cf73573e367475
DavisGilmore <davisgilmore@yahoo.com>
2017-10-20 14:29:54 -0400

    direct java file

---
+++ CreateTables.java
@@ -0,0 +1,36 @@
+package edu.rosehulman.gilmordw;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class CreateTables {
+	
+	public static void main(String[] args) throws IOException {
+		Configuration conf = HBaseConfiguration.create();
+		Connection connection = ConnectionFactory.createConnection(conf);
+		HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
+		
+		HTableDescriptor tableDes = new HTableDescriptor(TableName.valueOf("java_test_table"));
+		tableDes.addFamily(new HColumnDescriptor("test"));
+		
+		admin.createTable(tableDes);
+		
+		Table table = connection.getTable(TableName.valueOf("test"));
+		
+		Put p = new Put(Bytes.toBytes("test1"));
+		p.addColumn(Bytes.toBytes("test"), Bytes.toBytes("qualifier"), Bytes.toBytes("data"));
+		table.put(p);
+	}
+}
