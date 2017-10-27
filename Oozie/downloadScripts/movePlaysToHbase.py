73c839d3c1a216d703f4b6a542d844d980d185af
Joshua Laesch <laeschjs@rose-hulman.edu>
2017-10-27 13:17:00 -0400

    started oozie

--- Oozie/downloadScripts/movePlaysToHbase.py
+++ Oozie/downloadScripts/movePlaysToHbase.py
@@ -5,19 +5,20 @@ import shutil
 folder = sys.argv[1]
 files = os.listdir(folder)
 for file in files:
-	if file == "temp":
+	if os.path.isdir(folder + "/" + file):
 		continue
 	with open(folder + "/" + file) as f:
 		lines = f.readlines()
 		lines = [x.strip() for x in lines]
-		if os.path.exists(folder + "/temp"):
-			shutil.rmtree(folder + "/temp")
-		os.makedirs(folder + "/temp")
+		folderName = file.split(".")[0]
+		if not os.path.exists(folder + folderName):
+			os.makedirs(folder + folderName)
 		for line in lines:
 			if line.split(",")[0] == 'id':
 				filename = line.split(",")[1]
-			with open(folder + "/temp/" + filename, 'a+') as tempFile:
+			with open(folder + folderName + filename, 'a+') as tempFile:
 				tempFile.write(line + "\n");
+	continue
 	games = os.listdir(folder + "/temp")
 	for game in games:
 		os.system("pig -D tez.counters.max=1000 -x tez_local -param input='{}' -param gameId='{}' -param year='{}' -f goThroughEachGame.pig".format(folder + "/temp/" + game, game, sys.argv[2]))
