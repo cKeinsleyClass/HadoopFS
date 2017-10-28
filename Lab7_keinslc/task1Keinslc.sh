b5fc8e4e48973d0ffe881bcc7c2fb2cb7cf5e2dd
Christopher Keinsley <keinsleychris@gmail.com>
2017-10-28 13:30:57 -0400

    woops IK can't spell

--- Lab7_keinslc/task1Keinslc.sh
+++ Lab7_keinslc/task1Keinslc.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees --split-by eid --target-dir /tmp/sqoopOutput
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees --delete-target-dir --split-by eid --target-dir /tmp/sqoopOutput
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 1 --target-dir /tmp/sqoopMapOutput
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 1 --delete-target-dir --target-dir /tmp/sqoopMapOutput
 
-sqoop import --conect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --target-dir /tmp/sqoopSeqOutput --as-sequencefile
+sqoop import --conect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2  --delete-target-dir --target-dir /tmp/sqoopSeqOutput --as-sequencefile
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --warehourse-dir /tmp/sqoop/Employees 
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --delete-target-dir --warehouse-dir /tmp/sqoop/Employees 
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --null-string 'This is a NULL String' --fields-terminated-by '\t' --warehouse-dir /tmp/sqoop/Employees
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --delete-target-dir --null-string 'This is a NULL String' --fields-terminated-by '\t' --warehouse-dir /tmp/sqoop/Employees
 
 hive -e "create database if not exists sqooptest"
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --hive-import --crate-hive-table --hive-table sqooptest.Employees
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --hive-import --create-hive-table --hive-table sqooptest.Employees
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --null-string 'This is a NULL String' --fields-terminated-by '\t' --hive-import --crate-hive-table --hive-table sqooptest.Employees
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --null-string 'This is a NULL String' --fields-terminated-by '\t' --hive-import --create-hive-table --hive-table sqooptest.Employees
 
-sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username root -m 1 --warehouse-dir /tmp/sqoopAll
+sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username root -m 1 --delete-target-dir --warehouse-dir /tmp/sqoopAll
